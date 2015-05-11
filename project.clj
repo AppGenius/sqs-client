82312770d69271b6cbe8d44e08947e7c6a23b1fc
Benjamin Slawski <bslawski01@gmail.com>
2015-05-11 15:53:06 -0700

    maven config

--- project.clj
+++ project.clj
@@ -1,8 +1,16 @@
-(defproject sqs-client "0.0.0"
+(defproject sqs-client "0.0.0-SNAPSHOT"
   :description "tools for interfaceing with the AWS simple queue service"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cheshire "5.2.0"]
+  :repositories [["snapshots" {:url "s3p://ag-maven/snapshots"
+                               :username :env
+                               :passphrase :env}]
+                 ["releases" {:url "s3p://ag-maven/releases"
+                              :username :env
+                              :passphrase :env}]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [cheshire "5.2.0"]
                  [com.amazonaws/aws-java-sdk "1.6.10"]]
+  :plugins [[s3-wagon-private "1.1.2"]]
   :main sqs-client.core
   :aot [sqs-client.core])
